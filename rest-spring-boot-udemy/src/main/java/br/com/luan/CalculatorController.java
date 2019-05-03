f9a90777f104c265608278d6803b81284f34d320
Luan Andrade <luansandrade92@hotmail.com>
2019-05-03 11:21:35 -0300

    metodo sqrt implementado para calcular a raiz quadrada

--- rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
@@ -12,41 +12,51 @@ public class CalculatorController {
 
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo, false);
+        checkNumericValues(numberOne, numberTwo, false, false);
         return converToDouble(numberOne) + converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo, false);
+        checkNumericValues(numberOne, numberTwo, false, false);
         return converToDouble(numberOne) - converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double mul(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo, false);
+        checkNumericValues(numberOne, numberTwo, false, false);
         return converToDouble(numberOne) * converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo, true);
+        checkNumericValues(numberOne, numberTwo, true, false);
         return converToDouble(numberOne) / converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo, false);
+        checkNumericValues(numberOne, numberTwo, false, false);
         return (converToDouble(numberOne) + converToDouble(numberTwo))/2;
     }
 
-    private void checkNumericValues(String numberOne, String numberTwo, boolean division) throws Exception {
+    @RequestMapping(value="/sqrt/{numberOne}", method = RequestMethod.GET)
+    public Double sqrt(@PathVariable("numberOne") String numberOne) throws Exception {
+        checkNumericValues(numberOne, "0", false, true);
+        return Math.sqrt(converToDouble(numberOne));
+    }
+
+    private void checkNumericValues(String numberOne, String numberTwo, boolean isDivision, boolean isSquareroot) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Please set a numeric value.");
         }
 
-        if (division && converToDouble(numberTwo) == 0D) {
+        if (isDivision && converToDouble(numberTwo) == 0D) {
             throw new Exception("Number two can't be zero.");
+        }
+
+        if (isSquareroot && converToDouble(numberOne) < 0) {
+            throw new Exception("Number can't be less than zero.");
         }
     }
 

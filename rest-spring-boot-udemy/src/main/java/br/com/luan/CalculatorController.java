f3d2e2b7656cf42d5848152729c67276a6f67e0a
Luan Andrade <luansandrade92@hotmail.com>
2019-05-03 11:13:17 -0300

    metodo div implementado

--- rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
@@ -12,25 +12,35 @@ public class CalculatorController {
 
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo);
+        checkNumericValues(numberOne, numberTwo, false);
         return  converToDouble(numberOne) + converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo);
+        checkNumericValues(numberOne, numberTwo, false);
         return  converToDouble(numberOne) - converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double mul(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        checkNumericValues(numberOne, numberTwo);
+        checkNumericValues(numberOne, numberTwo, false);
         return  converToDouble(numberOne) * converToDouble(numberTwo);
     }
 
-    private void checkNumericValues(String numberOne, String numberTwo) {
+    @RequestMapping(value="/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        checkNumericValues(numberOne, numberTwo, true);
+        return  converToDouble(numberOne) / converToDouble(numberTwo);
+    }
+
+    private void checkNumericValues(String numberOne, String numberTwo, boolean division) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedOperationException("Please set a numeric value.");
+        }
+
+        if (division && converToDouble(numberTwo) == 0D) {
+            throw new Exception("Number two can't be zero.");
         }
     }
 

a10c591dd3803543a715287800aceab02f7a93bb
Luan Andrade <luansandrade92@hotmail.com>
2019-05-03 09:49:16 -0300

    verificando se é numérico e convertendo para double

--- rest-spring-boot-udemy/src/main/java/br/com/luan/GreetingController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/GreetingController.java
@@ -11,8 +11,18 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value="/sum/numberOne/numberTwo", method = RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return  converToDouble(numberOne) + converToDouble(numberTwo);;
+    }
 
+    private Double converToDouble(String number) {
         return 1D;
+    }
+
+    private boolean isNumeric(String number) {
+        return true;
     }
 }

8a22f5f2ddedda89add6e3e857b5d7e99c89d3a5
Luan Andrade <luansandrade92@hotmail.com>
2019-05-03 10:48:43 -0300

    renomeando controlador

--- rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
+public class CalculatorController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();

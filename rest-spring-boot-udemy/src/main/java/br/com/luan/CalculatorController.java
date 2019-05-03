6461c268347d79ef43976faeef5851e2c79d81bb
Luan Andrade <luansandrade92@hotmail.com>
2019-05-03 11:15:47 -0300

    implementado metodo mean para calcular a media

--- rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/CalculatorController.java
@@ -13,25 +13,31 @@ public class CalculatorController {
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         checkNumericValues(numberOne, numberTwo, false);
-        return  converToDouble(numberOne) + converToDouble(numberTwo);
+        return converToDouble(numberOne) + converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         checkNumericValues(numberOne, numberTwo, false);
-        return  converToDouble(numberOne) - converToDouble(numberTwo);
+        return converToDouble(numberOne) - converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double mul(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         checkNumericValues(numberOne, numberTwo, false);
-        return  converToDouble(numberOne) * converToDouble(numberTwo);
+        return converToDouble(numberOne) * converToDouble(numberTwo);
     }
 
     @RequestMapping(value="/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         checkNumericValues(numberOne, numberTwo, true);
-        return  converToDouble(numberOne) / converToDouble(numberTwo);
+        return converToDouble(numberOne) / converToDouble(numberTwo);
+    }
+
+    @RequestMapping(value="/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+        checkNumericValues(numberOne, numberTwo, false);
+        return (converToDouble(numberOne) + converToDouble(numberTwo))/2;
     }
 
     private void checkNumericValues(String numberOne, String numberTwo, boolean division) throws Exception {

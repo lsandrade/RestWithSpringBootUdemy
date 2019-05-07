629f4de90f142208153b04ad07fb71bf516a2044
Luan Andrade <luansandrade92@hotmail.com>
2019-05-07 14:35:23 -0300

    refactoring: criando mock person

--- rest-spring-boot-udemy/src/main/java/br/com/luan/services/PersonService.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/services/PersonService.java
@@ -13,28 +13,26 @@ public class PersonService {
     private final AtomicLong counter = new AtomicLong();
 
     public Person findById(String id) {
-
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Jon");
-        person.setLastName("Snow");
-        person.setAddress("Winterfel");
-        person.setGender("male");
-        return person;
+        return mockPerson(counter.incrementAndGet());
     }
 
     public List<Person> findAll() {
-        List<Person> persons = new ArrayList<Person>();
+        List<Person> persons = new ArrayList<>();
         for(int i=0; i<8; i++) {
-            Person person = new Person();
-            person.setId(i+1);
-            person.setLastName("Snow");
-            person.setFirstName("Jon");
-            person.setGender("male");
-            person.setAddress("Winterfel");
+            Person person = mockPerson((long) i+1);
             persons.add(person);
         }
         return persons;
+    }
+
+    private Person mockPerson(Long id) {
+        Person person = new Person();
+        person.setId(id);
+        person.setLastName("Snow");
+        person.setFirstName("Jon");
+        person.setGender("male");
+        person.setAddress("Winterfel");
+        return person;
     }
 
     public Person create(Person person) {

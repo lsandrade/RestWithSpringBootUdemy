c0a447d6827ac91b426dac82461a5ab516a91970
Luan Andrade <luansandrade92@hotmail.com>
2019-05-07 14:15:11 -0300

    findall person

--- rest-spring-boot-udemy/src/main/java/br/com/luan/controllers/PersonController.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/controllers/PersonController.java
@@ -4,20 +4,26 @@ import br.com.luan.model.Person;
 import br.com.luan.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
+import java.util.List;
 
 @RestController
-@RequestMapping("/oerson")
+@RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonService service;
 
-    @RequestMapping(value = "/id/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id){
         return service.findById(id);
     }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
 }
--- rest-spring-boot-udemy/src/main/java/br/com/luan/services/PersonService.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.luan.services;
 import br.com.luan.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
@@ -21,4 +23,17 @@ public class PersonService {
         return person;
     }
 
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for(int i=0; i<8; i++) {
+            Person person = new Person();
+            person.setId(i+1);
+            person.setLastName("Snow");
+            person.setFirstName("Jon");
+            person.setGender("male");
+            person.setAddress("Winterfel");
+            persons.add(person);
+        }
+        return persons;
+    }
 }

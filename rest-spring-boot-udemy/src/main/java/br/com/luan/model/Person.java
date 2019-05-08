912abfbfad0dd31f6305b3b374b504b740ca21de
Luan Andrade <luansandrade92@hotmail.com>
2019-05-08 13:33:01 -0300

    adicionadas annotations para comunicacao com db

--- rest-spring-boot-udemy/src/main/java/br/com/luan/model/Person.java
+++ rest-spring-boot-udemy/src/main/java/br/com/luan/model/Person.java
@@ -1,16 +1,29 @@
 package br.com.luan.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name="person")
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name="first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(length = 100)
     private String address;
+
+    @Column(length = 6)
     private String gender;
 
     @Override

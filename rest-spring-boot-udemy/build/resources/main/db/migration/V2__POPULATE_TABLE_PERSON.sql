d76d679b59f456ed0cf3d057ed4bb1b0ec9b2441
Luan Andrade <luansandrade92@hotmail.com>
2019-05-09 14:40:19 -0300

    migraçoes do banco

---
+++ rest-spring-boot-udemy/build/resources/main/db/migration/V2__POPULATE_TABLE_PERSON.sql
@@ -0,0 +1,6 @@
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Winterfel', 'Jon', 'male', 'Snow');
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Winterfel', 'Sansa', 'female', 'Stark');
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Winterfel', 'Arya', 'female', 'Stark');
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Kingslanding', 'Jaime', 'male', 'Lannister');
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Pyke', 'Theon', 'male', 'Greyjoy');
+INSERT INTO `rest_with_spring_boot_udemy`.`person`(`address`,`first_name`,`gender`,`last_name`) VALUES ('Kingslanding', 'Daenerys', 'female', 'Targaryen');
